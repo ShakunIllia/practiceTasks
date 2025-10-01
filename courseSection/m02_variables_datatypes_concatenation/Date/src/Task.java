2428e250ff08d1ece51bb02fff0d0f7a285a1ce4
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-10-01 09:32:35 +0300

    lab(2.1)_date

--- courseSection/m02_variables_datatypes_concatenation/Date/src/Task.java
+++ courseSection/m02_variables_datatypes_concatenation/Date/src/Task.java
@@ -1,7 +1,11 @@
 public class Task {
     public static void main(String[] args) {
-
-        //Write your solution here
-
+        int month = 10;
+        int day = 14;
+        int year = 2023;
+        System.out.print(month + "/");
+        System.out.print(day + "/");
+        System.out.print(year);
     }
 }
+
