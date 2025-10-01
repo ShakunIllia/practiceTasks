131124113b75242e520403a73bcfee662453014d
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-10-01 09:04:29 +0300

    first commit_

--- courseSection/m01_printing_escape_sequences/UserInfo/src/Task.java
+++ courseSection/m01_printing_escape_sequences/UserInfo/src/Task.java
@@ -1,7 +1,16 @@
 public class Task {
     public static void main(String[] args) {
+        String name = "First Name: Mike";
+        String lastName = "Last Name: Smith";
+        String city = "City: Fairfax";
+        String state = "State: VA";
+        String country = "Country: USA";
 
-        //Write your solution here
-
+        System.out.println(name);
+        System.out.println(lastName);
+        System.out.println();
+        System.out.println(city);
+        System.out.println(state);
+        System.out.println(country);
     }
 }
