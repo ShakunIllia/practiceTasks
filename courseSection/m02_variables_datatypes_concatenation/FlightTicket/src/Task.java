fd9c443d899e2a44eab0048d91f55448b9bc5bd2
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-10-07 22:49:47 +0300

    lab_2(FlightTicket)

--- courseSection/m02_variables_datatypes_concatenation/FlightTicket/src/Task.java
+++ courseSection/m02_variables_datatypes_concatenation/FlightTicket/src/Task.java
@@ -1,7 +1,10 @@
 public class Task {
     public static void main(String[] args) {
-
-        // Write your solution here
+        String city1 = "Chicago";
+        String city2 = "San Francisco";
+        double oneWayFare = 643.5;
+        char DOLLAR = '\u0024';
+        System.out.println("From " + city1 + " to " + city2 + " one way cost is " + DOLLAR + oneWayFare);
 
     }
 }
