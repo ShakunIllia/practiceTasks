92b2aab9d737e76cb0fda3e5134162d3f8c5f7c3
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-10-14 23:40:44 +0300

    lab_3(coins)

--- courseSection/m03_arithmetic_operators/CoinMachine/src/Task.java
+++ courseSection/m03_arithmetic_operators/CoinMachine/src/Task.java
@@ -8,7 +8,19 @@ public class Task {
 
         System.out.println("coins = " + coins);
 
-        //Add your solution here
-
+        int dollars = coins / 100;
+        coins = coins % 100;
+        int quarters = coins / 25;
+        coins = coins % 25;
+        int dimes = coins / 10;
+        coins = coins % 10;
+        int nickels = coins / 5;
+        coins = coins % 5;
+        int cents = coins;
+        System.out.println("dollars = "+dollars);
+        System.out.println("quarters = "+quarters);
+        System.out.println("dimes = "+dimes);
+        System.out.println("nickels = "+nickels);
+        System.out.println("cents = "+cents);
     }
 }
