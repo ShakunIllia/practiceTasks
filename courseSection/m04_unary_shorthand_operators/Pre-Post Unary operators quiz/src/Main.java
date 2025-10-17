ba9ea15f639f6cfda4145ff1a887c31db522c85c
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-10-17 23:15:30 +0300

    lab_4(Pre-post)

--- courseSection/m04_unary_shorthand_operators/Pre-Post Unary operators quiz/src/Main.java
+++ courseSection/m04_unary_shorthand_operators/Pre-Post Unary operators quiz/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
    public static void main(String[] args) {
-      // Write your solution here
+      // 1. 19
+      // 2. 20
+      // 3. 20
+      // 4. 40
+      System.out.println(19);
+      System.out.println("v = " + 20);
+      System.out.println("s = " + 20);
+      System.out.println("t = " + 40);
    }
 }
