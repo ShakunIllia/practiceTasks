9a7a42acd55c00a90e852d4385f935e4e0f54a19
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-10 23:05:44 +0200

    lab_6(dayOfWeek)

--- courseSection/m06_selection_statements/DayOfWeek/src/Task.java
+++ courseSection/m06_selection_statements/DayOfWeek/src/Task.java
@@ -6,7 +6,16 @@ public class Task {
         // ----Do not change below lines. needed for testing---
         day = args.length > 0 ? Integer.parseInt(args[0]) : day;
         //--------------------------------
-
-        //Write your solution here
+        System.out.println(day);
+        switch (day){
+            case 1: System.out.println("Monday"); break;
+            case 2: System.out.println("Tuesday"); break;
+            case 3: System.out.println("Wednesday"); break;
+            case 4: System.out.println("Thursday"); break;
+            case 5: System.out.println("Friday"); break;
+            case 6: System.out.println("Saturday"); break;
+            case 7: System.out.println("Sunday"); break;
+            default: System.out.println("No such a day"); break;
+        }
     }
 }
