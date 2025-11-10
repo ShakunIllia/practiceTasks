ffe99a69cc8e37d117bf96d8e807e7b4cfcdf6d0
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-10 22:57:29 +0200

    lab_6(response)

--- courseSection/m06_selection_statements/ConfirmationBot/src/Task.java
+++ courseSection/m06_selection_statements/ConfirmationBot/src/Task.java
@@ -6,9 +6,12 @@ public class Task {
         // ----Do not change below lines. needed for testing---
         response = args.length > 0 ? args[0].charAt(0) : response;
         //--------------------------------
-
-
-        //Write your solution here
-
+        System.out.println(response);
+        switch (response){
+            case 'y': System.out.println("Your request is being processed"); break;
+            case 'n': System.out.println("Thank you for your consideration"); break;
+            case 'h': System.out.println("Sorry, no live agents are currently available"); break;
+            default: System.out.println("Invalid entry, please try again"); break;
+        }
     }
 }
