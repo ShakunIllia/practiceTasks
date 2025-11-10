fdc52d736686d2a064e40a8c1dec18ddfad33378
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-10 23:21:09 +0200

    lab_6(finra)

--- courseSection/m06_selection_statements/Finra/src/Task.java
+++ courseSection/m06_selection_statements/Finra/src/Task.java
@@ -7,6 +7,17 @@ public class Task {
         number = args.length > 0 ? Integer.parseInt(args[0]) : 93;
         //--------------------------------
 
-        //Write your solution here
+        if(number % 3 == 0 && number % 5 == 0){
+            System.out.println("FINRA");
+        }
+        else if (number % 3 == 0){
+            System.out.println("FIN");
+        }
+        else if (number % 5 == 0){
+            System.out.println("RA");
+        }
+        else{
+            System.out.println(number);
+        }
     }
 }
