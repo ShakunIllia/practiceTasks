4a8ca4cc62f3ead0b8bff29cb85d513727d49fa7
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-12 20:57:43 +0200

    lab_6(gradeLevel)

--- courseSection/m06_selection_statements/Grades/src/Task.java
+++ courseSection/m06_selection_statements/Grades/src/Task.java
@@ -1,7 +1,7 @@
 public class Task {
     public static void main(String[] args) {
 
-        int testScore = 85;
+        int testScore = 79;
 
         // ----Do not change below lines. needed for testing---
         testScore = args.length > 0 ? Integer.parseInt(args[0]) : testScore;
@@ -9,7 +9,13 @@ public class Task {
 
         char grade;
 
-        //Write your solution here
+        if(testScore >= 90) {grade = 'A';}
+        else if(testScore >= 80) {grade = 'B';}
+        else if(testScore >= 70) {grade = 'C';}
+        else if(testScore >= 60) {grade = 'D';}
+        else {grade = 'E';}
+        System.out.println(grade);
+
 
     }
 }
