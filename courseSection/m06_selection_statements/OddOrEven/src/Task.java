73baaa301fba51248cc4123a74b55dc22c540464
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-12 22:34:43 +0200

    lab_6(evenOrOdd)

--- courseSection/m06_selection_statements/OddOrEven/src/Task.java
+++ courseSection/m06_selection_statements/OddOrEven/src/Task.java
@@ -7,8 +7,10 @@ public class Task {
         num = args.length > 0 ? Integer.parseInt(args[0]) : num;
         //--------------------------------
 
-        //Write your solution here
+        System.out.println("num is " + num);
 
+        if(num%2 == 0) {System.out.println(num + " is even");}
+        else System.out.println(num + " is odd");
 
     }
 }
