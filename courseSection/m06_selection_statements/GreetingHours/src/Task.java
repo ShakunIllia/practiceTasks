e883b2087e74ae962b6150ea9c5077c562769ac0
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-12 21:48:16 +0200

    lab_6(hour)

--- courseSection/m06_selection_statements/GreetingHours/src/Task.java
+++ courseSection/m06_selection_statements/GreetingHours/src/Task.java
@@ -1,12 +1,18 @@
 public class Task {
     public static void main(String[] args) {
 
-        int hour = 1;
+        int hour = 5;
 
         // ----Do not change below lines. needed for testing---
         hour = args.length > 0 ? Integer.parseInt(args[0]) : hour;
         //--------------------------------
 
-        //Write your solution here
+        System.out.println("hour is "+hour);
+
+        if(hour >= 5 && hour < 11) {System.out.println("Good Morning");}
+        else if(hour > 11 && hour < 18) {System.out.println("Good Afternoon");}
+        else if(hour >= 18 && hour <= 23 || hour >= 0 && hour < 5) {System.out.println("Good Evening");}
+        else System.out.println("Invalid hour");
+
     }
 }
