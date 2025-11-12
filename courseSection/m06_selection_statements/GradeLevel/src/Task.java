1b0fcfac48bf29ca67314bec60091fa44c0606ca
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-12 20:50:31 +0200

    lab_6(age)

--- courseSection/m06_selection_statements/GradeLevel/src/Task.java
+++ courseSection/m06_selection_statements/GradeLevel/src/Task.java
@@ -1,12 +1,20 @@
 public class Task {
     public static void main(String[] args) {
 
-        int age = 25;
+        int age = 10;
 
         // ----Do not change below lines. needed for testing---
         age = args.length > 0 ? Integer.parseInt(args[0]) : age;
         //--------------------------------
 
-        //Write your solution here
+        System.out.println("age is " + age);
+
+        if(age < 3) {System.out.println("ineligible");}
+        else if(age < 5) {System.out.println("preschool");}
+        else if(age == 5) {System.out.println("kindergarten");}
+        else if(age < 11) {System.out.println("elementary school");}
+        else if(age < 14) {System.out.println("middle school");}
+        else if(age < 18) {System.out.println("high school");}
+        else if(age > 18) {System.out.println("college");}
     }
 }
