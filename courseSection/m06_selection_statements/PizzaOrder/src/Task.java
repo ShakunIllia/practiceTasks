0e63eb9507e074693e91de2bdf811d777405da34
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-14 21:51:04 +0200

    lab_6(pizza)

--- courseSection/m06_selection_statements/PizzaOrder/src/Task.java
+++ courseSection/m06_selection_statements/PizzaOrder/src/Task.java
@@ -11,9 +11,29 @@ public class Task {
         extraCheese = args.length > 0 ? args[2].charAt(0) : extraCheese;
         //--------------------------------
 
-        double price = 0;
+        double price = 0.0;
 
-        //Write your solution here
+        System.out.println("Size: " + size);
+        System.out.println("Add Pepperoni: " + addPepperoni);
+        System.out.println("Extra Cheese: " + extraCheese);
 
+        if(size == 'S') {price += 13.99;}
+        else if(size == 'M') {price += 15.99;}
+        else if(size == 'L') {price += 18.79;}
+        else System.out.println("Invalid size");
+
+        if(addPepperoni == 'Y'){
+            if(size == 'S') {price += 1.50;}
+            else if(size == 'M') {price += 1.90;}
+            else if(size == 'L') {price += 2.10;}
+            else System.out.println("Invalid input");
+        }
+        if(extraCheese == 'Y'){
+            if(size == 'S') {price += 1.25;}
+            else if(size == 'M') {price += 1.65;}
+            else if(size == 'L') {price += 2.00;}
+            else System.out.println("Invalid input");
+        }
+        System.out.println("Total price " + price);
     }
 }
