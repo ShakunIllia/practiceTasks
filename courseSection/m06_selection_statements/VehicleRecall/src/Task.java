4745a9675fdf01e6b9b96b6d4e8f26d92e2ad83e
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-14 22:30:56 +0200

    lab_6(recall)

--- courseSection/m06_selection_statements/VehicleRecall/src/Task.java
+++ courseSection/m06_selection_statements/VehicleRecall/src/Task.java
@@ -7,8 +7,9 @@ public class Task {
         modelYear = args.length > 0 ? Integer.parseInt(args[0]) : modelYear;
         //--------------------------------
 
-        //Write your solution here
-
-
+        if ((modelYear >= 2001 && modelYear <= 2002) || (modelYear >= 2004 && modelYear <= 2006) || (modelYear >= 2015 && modelYear <= 2018) ) {
+            System.out.println("RECALL");
+        }
+        else System.out.println("NOT RECALL");
     }
 }
