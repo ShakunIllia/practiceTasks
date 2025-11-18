b270779ebae87ea439d6a9827b9caac0919c995f
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-18 20:14:09 +0200

    lab_7(050)

--- courseSection/m07_loops/From0To50/src/Task.java
+++ courseSection/m07_loops/From0To50/src/Task.java
@@ -1,7 +1,9 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        for(int i = 0; i<=50; i++){
+            System.out.println(i);
+        }
 
     }
 }
