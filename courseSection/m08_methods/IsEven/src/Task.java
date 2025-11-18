a71fc70dcb0f18ff125cd052240e1940f2ff3763
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-18 23:18:00 +0200

    lab_8(isEven)

--- courseSection/m08_methods/IsEven/src/Task.java
+++ courseSection/m08_methods/IsEven/src/Task.java
@@ -1,13 +1,13 @@
 public class Task {
     public static void main(String[] args) {
 
-        System.out.println(isEven(4));
+        System.out.println("isEven --> "+isEven(4));
 
     }
 
     public static boolean isEven(int n){
 
-        //Write your solution here
+        return n % 2 == 0;
 
     }
 
