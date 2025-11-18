db364fcbf309ef21361357ec56b2334ee0fde7ff
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-18 19:51:45 +0200

    lab_7(alphabet)

--- courseSection/m07_loops/Alphabet/src/Task.java
+++ courseSection/m07_loops/Alphabet/src/Task.java
@@ -1,7 +1,8 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
-
+        for(char i = 'A'; i <= 'Z'; i++) {System.out.print(i);}
+        System.out.println();
+        for(char i = 'z'; i >= 'a'; i--) {System.out.print(i);}
     }
 }
