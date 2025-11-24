afe01023366c9456d2b9400a4f361880a6cd954f
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-24 23:19:31 +0200

    lab_10(hi)

--- courseSection/m10_string/CountHi/src/Task.java
+++ courseSection/m10_string/CountHi/src/Task.java
@@ -7,8 +7,13 @@ public class Task {
 
     public static int countHi(String str) {
 
-        //Write your solution here
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == 'h' && str.charAt(i + 1) == 'i') {
+                count++;
+            }
 
+        }
+        return count;
     }
-
 }
