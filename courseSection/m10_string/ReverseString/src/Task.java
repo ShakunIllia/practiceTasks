c43b4dbd8db7df0719776215a10bc61a7c57675e
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-25 17:35:46 +0200

    lab_10(reversed)

--- courseSection/m10_string/ReverseString/src/Task.java
+++ courseSection/m10_string/ReverseString/src/Task.java
@@ -7,7 +7,13 @@ public class Task {
 
     public static String reverse(String str){
 
-        //Write your solution here
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        return reversed;
 
     }
 
