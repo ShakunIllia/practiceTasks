ab6a233520fd0d009e0a179432e77316ab2608b8
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-25 20:58:02 +0200

    lab_11(has55)

--- courseSection/m11_array/Has55/src/Task.java
+++ courseSection/m11_array/Has55/src/Task.java
@@ -6,8 +6,13 @@ public class Task {
     }
 
     public static boolean has55(int[] nums) {
-
-        //Write your solution here
-
+        boolean result = false;
+        for(int i = 0; i < nums.length -1; i++){
+            if(nums[i]==5 && nums[i+1]==5){
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
