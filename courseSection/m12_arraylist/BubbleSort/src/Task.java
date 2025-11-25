4d3ac4249ece4199a198a3c9785df7350c1e6641
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-25 23:40:20 +0200

    lab_12(bubbleSort)

--- courseSection/m12_arraylist/BubbleSort/src/Task.java
+++ courseSection/m12_arraylist/BubbleSort/src/Task.java
@@ -10,7 +10,16 @@ public class Task {
 
     public static ArrayList<Integer> sort(ArrayList<Integer> nums) {
 
-        //Write your solution here
+        for(int i = 0; i < nums.size()-1; i++){
+            for(int j =0; j < nums.size()-1-i; j++){
+                if(nums.get(j) > nums.get(j+1)){
+                    int temp = nums.get(j);
+                    nums.set(j, nums.get(j+1));
+                    nums.set(j+1, temp);
+                }
+            }
+        }
+        return nums;
 
     }
 
