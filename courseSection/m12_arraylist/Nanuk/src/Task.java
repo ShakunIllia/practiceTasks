bc22915492ecb97d8510a4312792b528afd58845
ShakunIllia <shakun.illia@chnu.edu.ua>
2025-11-26 00:04:07 +0200

    lab_12(hunt)

--- courseSection/m12_arraylist/Nanuk/src/Task.java
+++ courseSection/m12_arraylist/Nanuk/src/Task.java
@@ -4,19 +4,31 @@ import java.util.List;
 
 public class Task {
     public static void main(String[] args) {
-        System.out.println("nanuk([\"1\",\"2\",\"3\",\"5\"],0,5) -> " + hunt(new ArrayList<>(Arrays.asList("1","2","3","5")), 0, 5));
-        System.out.println("nanuk([\"1\",\"nanuk\",\"3\",\"5\"],0,5) -> " + hunt(new ArrayList<>(Arrays.asList("1","nanuk","3","5")), 0, 5));
-        System.out.println("nanuk([\"1\",\"3\"],0,5) -> " + hunt(new ArrayList<>(Arrays.asList("1","3")), 0, 5));
-        System.out.println("nanuk([\"1\",\"10\",\"3\",\"nanuk\"],2,7) -> " + hunt(new ArrayList<>(Arrays.asList("1","10","3","nanuk")), 2, 7));
-        System.out.println("nanuk([\"1\",\"10\",\"3\",\"nanuk\",\"nanuk\"],2,7) -> " + hunt(new ArrayList<>(Arrays.asList("1","10","3","nanuk","nanuk")), 2, 7));
-        System.out.println("nanuk([\"1\",\"10\",\"3\",\"nanuk\",\"nanuk\",\"nanuk\"],2,7) -> " + hunt(new ArrayList<>(Arrays.asList("1","10","3","nanuk","nanuk","nanuk")), 2, 7));
+        System.out.println("nanuk([\"1\",\"2\",\"3\",\"5\"],0,5) -> " + hunt(new ArrayList<>(Arrays.asList("1", "2", "3", "5")), 0, 5));
+        System.out.println("nanuk([\"1\",\"nanuk\",\"3\",\"5\"],0,5) -> " + hunt(new ArrayList<>(Arrays.asList("1", "nanuk", "3", "5")), 0, 5));
+        System.out.println("nanuk([\"1\",\"3\"],0,5) -> " + hunt(new ArrayList<>(Arrays.asList("1", "3")), 0, 5));
+        System.out.println("nanuk([\"1\",\"10\",\"3\",\"nanuk\"],2,7) -> " + hunt(new ArrayList<>(Arrays.asList("1", "10", "3", "nanuk")), 2, 7));
+        System.out.println("nanuk([\"1\",\"10\",\"3\",\"nanuk\",\"nanuk\"],2,7) -> " + hunt(new ArrayList<>(Arrays.asList("1", "10", "3", "nanuk", "nanuk")), 2, 7));
+        System.out.println("nanuk([\"1\",\"10\",\"3\",\"nanuk\",\"nanuk\",\"nanuk\"],2,7) -> " + hunt(new ArrayList<>(Arrays.asList("1", "10", "3", "nanuk", "nanuk", "nanuk")), 2, 7));
 
     }
 
     public static boolean hunt(ArrayList<String> result, int wayStones, int boast) {
 
-        //Write your solution here
+        int total = 0;
 
+        for(String s : result){
+            if(s.equals("nanuk")){
+                if (wayStones > 0){
+                    wayStones--;
+                } else {
+                    return false;
+                }
+            } else {
+                total += Integer.parseInt(s);
+            }
+        }
+
+        return total >= boast;
     }
-
 }
